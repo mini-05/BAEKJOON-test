ec6b5fd848d2b30bce4b39f11d2b1be231b32866
mini-05 <jen117@naver.com>
2022-05-16 15:33:37 +0900

    조건은 다 맞는 거 같은데...

--- baekjoon1358/baekjoon1358.cpp
+++ baekjoon1358/baekjoon1358.cpp
@@ -11,20 +11,23 @@ int main()
 		cin >> x[i] >> y[i];
 	for (int i = 0; i < p; i++)
 	{
-		if ((x[i] > x0 && y[i] >= y0) && (x[i] < x0 + w + h && y[i] <= y0 + w + h))
+		if ((x[i] >= x0 && y[i] >= y0) && (x[i] < x0 + w + h && y[i] <= y0 + w + h))
 		{
 			cnt++; // 네모박스 안에 있으면 cnt++
 			cout << "첫번째 실행\n";
+			cout << "실행 된 선수의 위치는: " << x[i] << "\t" << y[i] << "\n";
 		}
 		else if ((x0 <= x[i]) && (r < sqrt((x0 - x[i]) * (x0 - x[i]) + (y0 + r - y[i]) * (y0 + r - y[i]))))
 		{
 			cnt++;
 			cout << "두번째 실행\n";
+			cout << "실행 된 선수의 위치는: " << x[i] << "\t" << y[i] << "\n";
 		}
-		else if ((x0 >= x[i]) && (r < sqrt((x0 - x[i]) * (x0 - x[i]) + (y0 + r - y[i]) * (y0 + r - y[i]))))
+		else if ((x0 + w >= x[i]) && (r < sqrt((x0 - x[i]) * (x0 - x[i]) + (y0 + r - y[i]) * (y0 + r - y[i]))))
 		{
 			cnt++;
 			cout << "세번째 실행\n";
+			cout << "실행 된 선수의 위치는: " << x[i] << "\t" << y[i] << "\n";
 		}
 		else
 			continue;
