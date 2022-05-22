3be9249026d581e9ca003b61869cba707b4c0ba3
mini-05 <jen117@naver.com>
2022-05-23 01:39:37 +0900

    완성

--- baekjoon/2577/2577.cpp
+++ baekjoon/2577/2577.cpp
@@ -13,7 +13,41 @@ int main()
 	FILE* fp = freopen("input.txt", "r", stdin);
 #endif // LOCAL_TEST
 
-	int a, b, c, mul;
+	int a, b, c;
+	int count[10]{ 0 };
+	cin >> a >> b >> c;
+	int mul = a * b * c;
+
+	while (mul != 0)
+	{
+		count[mul % 10]++;	// mul에서 나머지 10을 통해 자릿수를 얻어 인덱스로 사용
+							// mul의 1의자리가 index가 된다.
+		mul /= 10;			// 매 회 자릿수를 줄이기 위해 10을 나눈다.
+							// index가 된 mul의 1의자리를 없앤다.
+	}
+
+	for (int v : count) // 0부터 9까지 count 배열을 출력한다. (foreach문 활용)
+		cout << v << "\n";
+
+	// %10과 index, foreach문 사용
+	/*int a, b, c;
+	int count[10]{ 0 };
+	cin >> a >> b >> c;
+	int mul = a * b * c;
+
+	while (mul != 0)
+	{
+		count[mul % 10]++; // mul에서 나머지 10을 통해 자릿수를 얻어 인덱스로 사용
+						   // mul의 1의자리가 index가 된다.
+		mul /= 10;		   // 매 회 자릿수를 줄이기 위해 10을 나눈다.
+						   // index가 된 mul의 1의자리를 없앤다.
+	}
+
+	for (int v : count) // 0부터 9까지 count 배열을 출력한다. (foreach문 사용)
+		cout << v << "\n"; //*/
+
+	// foreach문 활용
+	/*int a, b, c, mul;
 	int sum[10]{ 0 };
 	cin >> a >> b >> c;
 	mul = a * b * c;
@@ -24,7 +58,7 @@ int main()
 		sum[ch - '0']++; // 0의 ASCII = 48이고, arr[size]++는 arr[size]의 '값'을 ++한다.
 
 	for (int v : sum)
-		cout << v << "\n";
+		cout << v << "\n"; // */
 
 	return 0;
 }
