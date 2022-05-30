0dec81d44bf5a3a01d6a051e148ca8d6b1a84694
mini-05 <jen117@naver.com>
2022-05-30 22:54:37 +0900

    완성(주셕변경)

--- baekjoon/9012/9012.cpp
+++ baekjoon/9012/9012.cpp
@@ -1,29 +1,29 @@
-﻿//#include <iostream>
-//#include <vector>
-//#include <stack>
-//#include <string>
-//using namespace std;
-//int main()
-//{
-//	int iSize = 0;
-//	cin >> iSize;
-//
-//	for (int i = 0; i < iSize; ++i)
-//	{
-//		stack<char> st;
-//		string s;
-//		cin >> s;
-//
-//		for (int j = 0; j < s.length(); ++j)
-//		{
-//			if (st.empty() || s[j] == '(') st.push(s[j]);
-//			else if (st.top() == '(') st.pop();
-//		}
-//		if (st.empty()) cout << "YES" << endl;
-//		else cout << "NO" << endl;
-//	}
-//	return 0;
-//}
+﻿#include <iostream>
+#include <vector>
+#include <stack>
+#include <string>
+using namespace std;
+int main()
+{
+	int iSize = 0;
+	cin >> iSize;
+
+	for (int i = 0; i < iSize; ++i)
+	{
+		stack<char> st;
+		string s;
+		cin >> s;
+
+		for (int j = 0; j < s.length(); ++j)
+		{
+			if (st.empty() || s[j] == '(') st.push(s[j]);
+			else if (st.top() == '(') st.pop();
+		}
+		if (st.empty()) cout << "YES" << endl;
+		else cout << "NO" << endl;
+	}
+	return 0;
+}
 
 
 
